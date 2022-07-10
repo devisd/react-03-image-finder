3455d0223e0cd50b65b87d5436dd445e80abe74e
devisd <89788799+devisd@users.noreply.github.com>
2022-07-11 04:08:04 +0700

    test1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Searchbar from 'components/Searchbar';
-import Loader from 'components/Loader';
 import ImageGallery from 'components/ImageGallery';
-import Button from 'components/Button';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,10 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 class App extends Component {
   state = {
-    images: null,
     query: '',
-    page: 1,
-    loading: false,
   };
 
   // static propTypes = {
@@ -24,25 +19,8 @@ class App extends Component {
   //   loading: PropTypes.bool,
   // };
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query } = this.state;
-    if (prevState.query !== this.state.query) {
-      this.setState({
-        query,
-        page: 1,
-      });
-    }
-  }
-
-  onLoadMore = page => {
-    this.setState({
-      page,
-    });
-  };
-
   formSubmitHandler = data => {
     const searchQuery = data.toLowerCase();
-    const { page } = this.state;
 
     if (searchQuery.trim() === '') {
       return toast.error('Введите название для поиска');
@@ -50,34 +28,17 @@ class App extends Component {
 
     this.setState({
       query: searchQuery,
-      loading: true,
     });
-
-    setTimeout(
-      () =>
-        fetch(
-          `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=28430104-64039a230ce7799bb0faf758d&image_type=photo&orientation=horizontal&per_page=12`
-        )
-          .then(response => response.json())
-          .then(data => {
-            this.setState({ images: data.hits });
-          })
-          .finaly(this.setState({ loading: false })),
-      3000
-    );
   };
 
   render() {
-    const { images, loading } = this.state;
+    const { query } = this.state;
 
     return (
       <div>
         <Searchbar onSubmit={this.formSubmitHandler} />
-        {images && <ImageGallery data={images} />}
+        <ImageGallery query={query} />
 
-        {images && <Button onClick={this.onLoadMore} />}
-
-        {loading && <Loader />}
         <ToastContainer autoClose={2500} theme="dark" />
       </div>
     );
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ImageGalleryItem from 'components/ImageGalleryItem';
+import Button from 'components/Button';
+import Loader from 'components/Loader';
 import css from './ImageGallery.module.css';
 
-const ImageGallery = ({ data }) => {
-  if (data.length === 0) {
-    return <h1 style={{ textAlign: 'center' }}>Нет картинок с такой темой</h1>;
-  }
-  return (
-    <div>
-      <ul className={css.ImageGallery}>
-        <ImageGalleryItem data={data} />
-      </ul>
-    </div>
-  );
-};
+export default class ImageGallery extends Component {
+  state = {
+    images: null,
+    loading: false,
+    page: 1,
+  };
 
-export default ImageGallery;
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.query !== this.props.query) {
+      this.setState({
+        loading: true,
+      });
+      // setTimeout(() => {
+      fetch(
+        `https://pixabay.com/api/?q=${this.props.query}&page=1&key=28430104-64039a230ce7799bb0faf758d&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            images: data.hits,
+          });
+        })
+        .finally(() => this.setState({ loading: false }));
+      // }, 3000);
+    }
+  }
+
+  onLoadMore = page => {
+    this.setState({
+      page,
+    });
+  };
+
+  render() {
+    const { images, loading } = this.state;
+
+    return (
+      <div>
+        <ul className={css.ImageGallery}>
+          {images && <ImageGalleryItem data={images} />}
+        </ul>
+        {loading && <Loader />}
+
+        {images && <Button onClick={this.onLoadMore} />}
+      </div>
+    );
+  }
+}
